23aaf4f669f13abc2c54d78b4de5ad2ddaa9ac16
Jaeha Yi <jaehaha1@gmail.com>
2024-11-24 02:00:47 -0700

    digit button implementation started, partially working

--- javascript.js
+++ javascript.js
@@ -37,6 +37,17 @@ function operate(mem, op, input) {
     }
 }
     
+const buttons = document.querySelectorAll(".digit");
+
+console.log(buttons)
+
+buttons.forEach((button) => {
+    // and for each one we add a 'click' listener
+    button.addEventListener("click", () => {
+        console.log(button.textContent);
+    });
+});
+
 
 console.log(operate(mem, op, input))
 console.log(add(mem, input))
